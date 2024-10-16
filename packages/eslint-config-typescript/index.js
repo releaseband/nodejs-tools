e73d2ade3dd2321ccff2bc4982e554e9d670c8b7
neyman aleksandr <neyman.sashka@gmail.com>
2024-10-16 18:49:43 +0300

    feat(eslint-config-typescript)!: eslint9 typescript config migration

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -1,31 +1,83 @@
-module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'airbnb-base',
-    'airbnb-typescript/base',
-    'plugin:prettier/recommended',
-    'plugin:sonarjs/recommended',
-  ],
-  plugins: ['simple-import-sort', 'tsdoc'],
-  rules: {
-    'import/prefer-default-export': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: [
-          '**/*.setup.js',
-          '**/*.setup.ts',
-          '**/*.config.js',
-          '**/*.config.ts',
-          '**/*.test.ts',
-          '**/*.spec.ts',
-        ],
+import eslint from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import importPlugin from 'eslint-plugin-import';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import sonarjs from 'eslint-plugin-sonarjs';
+import tsdoc from 'eslint-plugin-tsdoc';
+import tseslint from 'typescript-eslint';
+
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  sonarjs.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        project: './tsconfig.json',
       },
+    },
+    plugins: {
+      tsdoc,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-require-imports': 'warn',
+      'tsdoc/syntax': 'warn',
+    },
+    ignores: [
+      '**/node_modules/*',
+      '**/dist/*',
+      '**/build/*',
+      '**/public/*.js',
+      '**/.parcel-cache/*',
+      '**/.idea/*',
+      '**/.vscode/*',
+      '**.history/*',
+      '*.sublime-project',
+      '*.sublime-workspace',
     ],
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
-    'tsdoc/syntax': 'warn',
   },
-};
+  {
+    files: ['.lintstagedrc.js'],
+    rules: {
+      'no-undef': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,tsx,jsx}'],
+    rules: {
+      'sonarjs/todo-tag': 'off',
+      'sonarjs/no-commented-code': 'off',
+      'sonarjs/deprecation': 'warn',
+      'import/no-named-as-default-member': 'off',
+      'import/no-unresolved': 'off',
+      'import/named': 'off',
+      'import/prefer-default-export': 'off',
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: [
+            '**/*.setup.js',
+            '**/*.config.js',
+            '**/*.test.js',
+            '**/*.spec.js',
+            'vite.config.ts',
+            '.lintstagedrc.js',
+          ],
+        },
+      ],
+      'import/order': ['error', { alphabetize: { order: 'asc', caseInsensitive: true } }],
+    },
+  },
+];
